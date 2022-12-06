cfd113a8485761fa3d2c4591bd7db6ca4f963399
Vignesh03022001 <shrivigneshprasanna@gmail.com>
2022-12-06 11:00:07 +0530

    user_dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserDashBoard from './userPages/userDashboard';
+
+function App() {
+  return (
+    <div >
+      <UserDashBoard />
+    </div>
+  );
+}
+
+export default App;
